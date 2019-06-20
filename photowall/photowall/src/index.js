b3b07728fec9281301fc01ce00057160dfc1a66e
yfjd01 <yfjd01@gmail.com>
2019-06-19 21:30:17 -0400

    Elements without JSX

--- photowall/photowall/src/index.js
+++ photowall/photowall/src/index.js
@@ -1,8 +1,15 @@
 import React from 'react' //how to import react library, give access to libraries methods
 import ReactDOM from 'react-dom'
 
+const tasks = ['Take out the trash', 'Shovel the drive way', 'Walk the dog'];
+
 //reacts make use of elements to describe ui, elements are objects duh
-const element = React.createElement('h1', null, 'Hello World!'); //1st argument is tag, 2nd is props, 3rd is content
+//1st argument is tag, 2nd is props, 3rd is content
+const element = React.createElement('ol', null, //ol stands for ordered list tag
+    tasks.map((task, index) => React.createElement('li', {key: index}, task))
+);
+
+//nesting elements can be pretty tedious, the solution is JSX
 
 ReactDOM.render(element, document.getElementById('root')); //actually rendering the element onto webpage
 //root element of the document is discributed in public/index.html 
