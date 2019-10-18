7763a14db7490641971f07a5e3aecac27a360ca1
yfjd01 <yfjd01@gmail.com>
2019-10-17 23:07:16 -0400

    Finished inital photowall and moved notes aroind, provided comments on how CSS works and how passing props works in React

--- photowall/photowall/src/index.js
+++ photowall/photowall/src/index.js
@@ -1,9 +1,9 @@
 import React, {Component} from 'react' 
 //how to import react library, give access to libraries methods
 //we can make component by importing Complent class from react
-
 import Main from './Components/Main'
 import ReactDOM from 'react-dom'
+import './styles/stylesheet.css'
 
 //This is the compositional model, we combine subcomponents to make a main component
 //This is exactly what we had before but we are rendering components this time
